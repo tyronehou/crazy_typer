021adc546d0d81472c53950df8751284bbca947b
Tyrone Hou <tyroneh@bu.edu>
2016-02-23 23:18:00 -0500

    Fixed firefox driver connection refused error

--- crazytyper.py
+++ crazytyper.py
@@ -1,3 +1,5 @@
+# Currently only works with 10fastfingers.com typing test
+
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
@@ -5,49 +7,59 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 from selenium.webdriver.common.by import By
 import time
 
-# TODO: Add a random typer (poisson distributed?!) Hell yes
+# TODO: Add a random typer (exponential distributed interarrival times)
+
+class DriverNotFoundException(Exception):
+    pass
 
 def interval(wpm):
     ''' determines the average time in seconds between words for
         a given rate in words per minute (wpm) '''
     return 60 / wpm
 
-# Create a new instance of the Firefox driver
-driver = webdriver.Firefox()
+class typer:
 
-# go to the google home page
-driver.get("http://10fastfingers.com/typing-test/english")
+    def __init__(self, browser, speed = 60):
+        # Create a new instance of the specified driver
+        if browser == 'HTML':
+            self.driver = webdriver.Remote(desired_capabilities=webdriver.
+                                           DesiredCapabilities.HTMLUNIT.copy())
+        elif browser == 'Firefox':
+            self.driver = webdriver.Firefox()
+        else:
+            raise DriverNotFoundException
 
-# the page is ajaxy so the title is originally this:
-print(driver.title)
+    def run(self):
+        self.driver.get("http://10fastfingers.com/typing-test/english")
 
-try:
-    wait = WebDriverWait(driver, 10)
-    inputElement = wait.until(EC.presence_of_element_located((By.ID, "inputfield")))
-    word_list = driver.execute_script("return document.getElementById('wordlist').innerHTML")
+        print(self.driver.title)
+        
+        try:
+            wait = WebDriverWait(self.driver, 10)
+            inputElement = wait.until(EC.presence_of_element_located((By.ID, "inputfield")))
+            word_list = self.driver.execute_script("return document.getElementById('wordlist').innerHTML")
 
-    # maximum speed (all input at once)
-    # words = word_list.replace('|', ' ') + ' '
-    # inputElement.send_keys(words)
+            # maximum speed (all input at once)
+            words = word_list.replace('|', ' ') + ' '
+            inputElement.send_keys(words)
 
-    # input word by word
-    # words = word_list.split('|')
-    # for word in words:
-    #     inputElement.send_keys(word + " ")
+            # input word by word
+            #words = word_list.split('|')
+            #for word in words:
+            #     inputElement.send_keys(word + " ")
 
-    words = word_list.replace('|', ' ') + ' '
+            words = word_list.replace('|', ' ') + ' '
 
-    print(words)
-    print('#:', len(words.split()))
+            print(words)
+            print('#:', len(words.split()))
 
-    start_typing = time.time()
-    input('Press enter')
-    inputElement.send_keys(words)
-    stop_typing = time.time()
-    
-    seconds_taken = (stop_typing - start_typing) / 1000
-    wpm = len(words) / seconds_taken
-finally:
-    print('time:', seconds_taken)
-    print('actual wpm:', wpm)
-    #driver.quit()
+            input('Press enter')
+            inputElement.send_keys(words)
+            
+
+        finally:
+            driver.quit()
+
+
+if __name__ == '__main__':
+    typer('Firefox').run()
